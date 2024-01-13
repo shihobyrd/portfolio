0929fa30da19f21f4a0ba5054f92b09849ee0d6d
Shiho Byrd <shihobyrd.m25@gmail.com>
2024-01-12 19:30:31 -0800

    projects filter-btn working

---
+++ filter.js
@@ -0,0 +1,23 @@
+let filterBtn = document.querySelectorAll('.filter-btn');
+let itemBox = document.querySelectorAll('.itemBox');
+
+for (let i = 0; i < filterBtn.length; i++) {
+    filterBtn[i].addEventListener('click', function () {
+        for (let j = 0; j < filterBtn.length; j++) {
+            filterBtn[j].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        let dataFilter = this.getAttribute('data-filter');
+
+        for (let k = 0; k < itemBox.length; k++) {
+            itemBox[k].classList.remove('active');
+            itemBox[k].classList.add('hide');
+
+            if (itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
+                itemBox[k].classList.remove('hide');
+                itemBox[k].classList.add('active');
+            }
+        }
+    })
+}
